9e36486223a4d5f2946603bb263563aa3e792f36
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2017-05-22 22:07:50 +0200

    Added a new details controller action method and new details page.

---
+++ ToDoListMVC/Controllers/ItemsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ToDoListMVC.DAL;
+
+namespace ToDoListMVC.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private ToDoListEntitiesContext db = new ToDoListEntitiesContext();
+
+        // GET: Items
+        public ActionResult Index()
+        {
+            var items = db.Items.ToList();
+
+            return View(items);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var details = db.Items.FirstOrDefault(items => items.ItemId == id);
+
+            return View(details);
+        }
+    }
+}
